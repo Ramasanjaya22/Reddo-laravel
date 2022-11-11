e92f85ea8c31cb78969e8589b1d517bc90d52f3f
alan1331 <sahlan.royale@gmail.com>
2022-11-12 01:33:58 +0700

    add google login beta

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 // front ( landing )
 use App\Http\Controllers\Landing\LandingController;
+use App\Http\Controllers\GoogleLoginController;
 
 // member ( dashboard )
 use App\Http\Controllers\Dashboard\MemberController;
@@ -11,6 +12,8 @@ use App\Http\Controllers\Dashboard\LeaderboardController;
 use App\Http\Controllers\Dashboard\BooksController;
 use App\Http\Controllers\Dashboard\ReminderController;
 use App\Http\Controllers\Dashboard\ProfileController;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 /*
 |--------------------------------------------------------------------------
@@ -51,3 +54,6 @@ Route::group(['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sa
     Route::resource('profile', ProfileController::class);
 
 });
+
+Route::get('auth/google', [GoogleLoginController::class, 'redirectToGoogle']);
+Route::get('auth/google/callback', [GoogleLoginController::class, 'handleGoogleCallback']);
